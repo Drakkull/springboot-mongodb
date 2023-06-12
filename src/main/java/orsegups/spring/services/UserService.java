bb63ef4977b81dd15517b3fa5260639ede1cd8c4
Leonardo DiAbreu <drakkull007@hotmail.com>
2023-06-12 15:26:41 -0300

    User Update using PUT added

--- src/main/java/orsegups/spring/services/UserService.java
+++ src/main/java/orsegups/spring/services/UserService.java
@@ -34,9 +34,20 @@ public class UserService {
 
 
     //On video class told to use repo.delete(id) but doesn't work, only deleteById
-    public void delete(String id){
+    public void delete(String id) {
         findById(id);
         repo.deleteById(id);
+    }
+
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
     }
 
 
