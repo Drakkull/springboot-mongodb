5f5cd0305243f6d4a850b3c47354e43a46a17d06
Leonardo DiAbreu <drakkull007@hotmail.com>
2023-06-13 16:41:52 -0300

    Getting Post by ID

---
+++ src/main/java/orsegups/spring/resources/PostResource.java
@@ -0,0 +1,36 @@
+package orsegups.spring.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import orsegups.spring.domain.Post;
+import orsegups.spring.domain.User;
+import orsegups.spring.dto.UserDTO;
+import orsegups.spring.services.PostService;
+import orsegups.spring.services.UserService;
+
+import java.lang.reflect.Array;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+
+
+    @RequestMapping(value ="/{id}", method = RequestMethod.GET)
+    public ResponseEntity <Post> findById(@PathVariable String id){
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+
+}
---
+++ src/main/java/orsegups/spring/services/PostService.java
@@ -0,0 +1,29 @@
+package orsegups.spring.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import orsegups.spring.domain.Post;
+import orsegups.spring.domain.User;
+import orsegups.spring.dto.UserDTO;
+import orsegups.spring.repository.PostRepository;
+import orsegups.spring.repository.UserRepository;
+import orsegups.spring.services.exceptions.ObjectNotFoundException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!!!"));
+    }
+
+
+}
