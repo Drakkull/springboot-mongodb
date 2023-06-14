378785af050ee3b1532a44a102e130ce89f3153b
Leonardo DiAbreu <drakkull007@hotmail.com>
2023-06-14 16:05:04 -0300

    Simple consult now using @Query

--- src/main/java/orsegups/spring/repository/PostRepository.java
+++ src/main/java/orsegups/spring/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package orsegups.spring.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 import orsegups.spring.domain.Post;
 
@@ -8,6 +9,9 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': { $regex:  ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
     //findByTitleContaining is case sensitive, but you can Ignore as follows below
     List<Post> findByTitleContainingIgnoreCase(String text);
